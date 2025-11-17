2a5293c011b987de793c08e34f9f742992755aad
Gauravyadav772 <gauravipin700@gmail.com>
2025-11-18 00:36:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (65.74%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -3,18 +3,15 @@ public:
     int search(vector<int>& nums, int target) {
         int s = 0;
         int e = nums.size() - 1;
-
         while (s <= e) {
             int mid = s + (e - s) / 2;
-
             if (nums[mid] == target)
                 return mid;
-
             if (nums[s] <= nums[mid]) {
-
                 if (target >= nums[s] && target < nums[mid]) {
                     e = mid - 1;
-                } else {
+                } 
+                else {
                     s = mid + 1;
                 }
             }
@@ -27,7 +24,6 @@ public:
                 }
             }
         }
-
-        return -1;
+         return -1;
     }
 };
